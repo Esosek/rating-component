c978847df8b819a1545cff74327165d6c9874220
Ales Zima <ales@cpex.cz>
2024-01-26 16:48:54 +0100

    Added transition animation

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const thanksContent = document.getElementById('thanks-content');
 const selectedRating = document.querySelector('.selected-rating');
 
 submitBtn.addEventListener('click', () => {
-  ratingContent.classList.toggle('hidden');
-  thanksContent.classList.toggle('hidden');
+  ratingContent.classList.add('hidden');
+  thanksContent.classList.remove('hidden');
+  setTimeout(() => {
+    thanksContent.classList.add('open');
+  }, 100);
 });
 
 for (let i = 0; i < ratingBtns.length; i++) {
