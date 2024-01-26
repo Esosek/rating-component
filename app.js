32b1cffdc3fb5919113ea46f58f86fc5288a11b9
Ales Zima <ales@cpex.cz>
2024-01-26 16:35:38 +0100

    Completed

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const submitBtn = document.querySelector('.submit-btn');
+const ratingBtns = document.getElementsByClassName('rating-btn');
 const ratingContent = document.getElementById('rating-content');
 const thanksContent = document.getElementById('thanks-content');
+const selectedRating = document.querySelector('.selected-rating');
 
 submitBtn.addEventListener('click', () => {
   ratingContent.classList.toggle('hidden');
   thanksContent.classList.toggle('hidden');
 });
+
+for (let i = 0; i < ratingBtns.length; i++) {
+  ratingBtns[i].addEventListener('click', () => {
+    selectedRating.innerHTML = (i + 1).toString();
+  });
+}
